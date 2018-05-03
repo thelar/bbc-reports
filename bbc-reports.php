f5ded279356434ca3216bb9235091b8d5af3898f
Kevin Price-Ward <kevin.price-ward@chapteragency.co.uk>
2018-05-03 13:02:17 +0100

    version 1.9

--- bbc-reports.php
+++ bbc-reports.php
@@ -3,7 +3,7 @@
 Plugin Name: BBC Reports
 Description: Custom plugin to export reports from Jewson Building Better Communities
 Author: Kevin Price-Ward
-Version: 1.8
+Version: 1.9
 */
 
 add_action('admin_menu', 'bbc_reports_menu_page');
@@ -207,6 +207,8 @@ function _handle_form_action(){
         $user_args = [];
         if($role!==''){
             $user_args['role'] = $role;
+        }else{
+            $user_args['role'] = 'all'
         }
         $users = get_users($user_args);
         var_dump($users);
